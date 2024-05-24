eb56425529d2f00eaeda248a4250eefe8e5c49ff
mariadmc <mmorcob238@g.educaand.es>
2024-05-24 13:42:53 +0200

    Controlador de proyecto creado

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\AcademicYear;
 use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProjectController
+class ProjectController extends AbstractController
 {
     #[Route('/project', name: 'project')]
     final public function list (ProjectRepository $projectRepository): Response

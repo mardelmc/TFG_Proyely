8d6a1f6fe85986ed89bb77dadac00fd3701b3a98
mariadmc <mmorcob238@g.educaand.es>
2024-05-31 12:04:31 +0200

    Migracion inicial

---
+++ migrations/Version20240531111017.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240531111017 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE academic_year (id INT AUTO_INCREMENT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `group` (id INT AUTO_INCREMENT NOT NULL, tutor_id INT NOT NULL, academic_year_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_6DC044C5208F64F1 (tutor_id), INDEX IDX_6DC044C5C54F3401 (academic_year_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE group_subject (group_id INT NOT NULL, subject_id INT NOT NULL, INDEX IDX_7DCE6A76FE54D947 (group_id), INDEX IDX_7DCE6A7623EDC87 (subject_id), PRIMARY KEY(group_id, subject_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, student_id INT NOT NULL, proposed_by_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_2FB3D0EECB944F1A (student_id), INDEX IDX_2FB3D0EEDAB5A938 (proposed_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, group_id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, mark DOUBLE PRECISION DEFAULT NULL, INDEX IDX_B723AF33FE54D947 (group_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student_subject (student_id INT NOT NULL, subject_id INT NOT NULL, INDEX IDX_16F88B82CB944F1A (student_id), INDEX IDX_16F88B8223EDC87 (subject_id), PRIMARY KEY(student_id, subject_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE subject (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE teacher (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, tutor TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE teacher_academic_year (teacher_id INT NOT NULL, academic_year_id INT NOT NULL, INDEX IDX_EF1B695541807E1D (teacher_id), INDEX IDX_EF1B6955C54F3401 (academic_year_id), PRIMARY KEY(teacher_id, academic_year_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nickname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `group` ADD CONSTRAINT FK_6DC044C5208F64F1 FOREIGN KEY (tutor_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE `group` ADD CONSTRAINT FK_6DC044C5C54F3401 FOREIGN KEY (academic_year_id) REFERENCES academic_year (id)');
+        $this->addSql('ALTER TABLE group_subject ADD CONSTRAINT FK_7DCE6A76FE54D947 FOREIGN KEY (group_id) REFERENCES `group` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE group_subject ADD CONSTRAINT FK_7DCE6A7623EDC87 FOREIGN KEY (subject_id) REFERENCES subject (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EECB944F1A FOREIGN KEY (student_id) REFERENCES student (id)');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEDAB5A938 FOREIGN KEY (proposed_by_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33FE54D947 FOREIGN KEY (group_id) REFERENCES `group` (id)');
+        $this->addSql('ALTER TABLE student_subject ADD CONSTRAINT FK_16F88B82CB944F1A FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE student_subject ADD CONSTRAINT FK_16F88B8223EDC87 FOREIGN KEY (subject_id) REFERENCES subject (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE teacher_academic_year ADD CONSTRAINT FK_EF1B695541807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE teacher_academic_year ADD CONSTRAINT FK_EF1B6955C54F3401 FOREIGN KEY (academic_year_id) REFERENCES academic_year (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `group` DROP FOREIGN KEY FK_6DC044C5208F64F1');
+        $this->addSql('ALTER TABLE `group` DROP FOREIGN KEY FK_6DC044C5C54F3401');
+        $this->addSql('ALTER TABLE group_subject DROP FOREIGN KEY FK_7DCE6A76FE54D947');
+        $this->addSql('ALTER TABLE group_subject DROP FOREIGN KEY FK_7DCE6A7623EDC87');
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EECB944F1A');
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EEDAB5A938');
+        $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF33FE54D947');
+        $this->addSql('ALTER TABLE student_subject DROP FOREIGN KEY FK_16F88B82CB944F1A');
+        $this->addSql('ALTER TABLE student_subject DROP FOREIGN KEY FK_16F88B8223EDC87');
+        $this->addSql('ALTER TABLE teacher_academic_year DROP FOREIGN KEY FK_EF1B695541807E1D');
+        $this->addSql('ALTER TABLE teacher_academic_year DROP FOREIGN KEY FK_EF1B6955C54F3401');
+        $this->addSql('DROP TABLE academic_year');
+        $this->addSql('DROP TABLE `group`');
+        $this->addSql('DROP TABLE group_subject');
+        $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE student');
+        $this->addSql('DROP TABLE student_subject');
+        $this->addSql('DROP TABLE subject');
+        $this->addSql('DROP TABLE teacher');
+        $this->addSql('DROP TABLE teacher_academic_year');
+        $this->addSql('DROP TABLE user');
+    }
+}

3f8b2fe15992f968a2c375c467a805294b9ba516
mardelmc <mmorcob238@g.educaand.es>
2024-11-30 17:21:40 +0100

    Creado Type de Student y Teacher

---
+++ src/Form/TeacherType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Teacher;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeacherType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nickname')
+            ->add('password')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('tutor')
+            ->add('tutoredGroup')
+            ->add('academicYears')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Teacher::class,
+        ]);
+    }
+}

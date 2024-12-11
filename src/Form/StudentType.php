39b82c2eb6ccd91fd6d8cbf135a95502ea401444
mardelmc <mmorcob238@g.educaand.es>
2024-12-11 14:06:14 +0100

    Arreglo de contraseñas estudiante

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -4,16 +4,28 @@ namespace App\Form;
 
 use App\Entity\Student;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class StudentType extends AbstractType
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nickname')
-            ->add('password')
+            ->add('password', PasswordType::class, [
+                'required' => $options['is_new'],
+            ])
             ->add('firstName')
             ->add('lastName')
             ->add('mark')
@@ -21,12 +33,23 @@ class StudentType extends AbstractType
             ->add('project')
             ->add('subjects')
         ;
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            /** @var Student $student */
+            $student = $event->getData();
+
+            if ($student->getPassword()) {
+                $hashedPassword = $this->passwordHasher->hashPassword($student, $student->getPassword());
+                $student->setPassword($hashedPassword);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Student::class,
+            'is_new' => true,
         ]);
     }
 }
